9a9ada0bad0df199b9bb581d74f67163586aa3ca
Lucas klafke <lucasnkz11@gmail.com>
2022-06-10 01:34:05 -0300

    feat: get users/:id endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -4,15 +4,35 @@ export async function getUser(req,res){
         const { id }= req.params
         const {user} = res.locals
 
-        if(user !== id){
+
+        if(Number(user) !== Number(id)){
                 return res.sendStatus(401)
         }
 
         try{
-                const result = connection.query(
-                        `SELECT * FROM users`
-                )
-                res.send(result)
+                // PRIMEIRO PEGA AS INFOS DO USER E DEPOIS PEGA AS SHORTS URLS DELE
+                const resultUser = await connection.query(
+                  `SELECT users.id, users.name, sum(su."visitCount") as "visitCount" from users
+                        join sections
+                                on users.id = sections."userId"
+                        join shortened_urls as su
+                                on sections.id = su."sectionId"
+                        where users.id = $1        
+                        group by users.id`,
+                  [id]
+                );
+                if(resultUser.rowCount === 0){
+                        return res.sendStatus(404)
+                }
+                const resultShortUrls = await connection.query(
+                `SELECT su.id, su.url, su."shortenedUrl", su."visitCount" from shortened_urls  as su
+                        join sections
+                                on su."sectionId" = sections.id
+                        where sections."userId" = $1  `,[id]
+                );
+
+                const response = _mapShortenedUrls(resultUser, resultShortUrls)
+                res.send(response);
         }catch(err){
                 res.send(err)
         }
@@ -41,6 +61,19 @@ export async function getRanking(req,res){
 }
 
 
-// function _map{
+function _mapShortenedUrls(user,shortened_urls){
+        const {rows} = shortened_urls
+        const {id, name, visitCount } = user.rows[0]
+        
+        const shortenedUrls = rows.map( e => {
+                return e
+        })
+        const response = {
+          id,
+          name,
+          visitCount,
+          shortenedUrls
+        };
+        return response
 
-// }
+}
