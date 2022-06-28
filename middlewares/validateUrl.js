dbee291dcabf44d87eaa7de834501162c5f2304b
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-06-28 10:48:35 -0300

    Preparando para revisão de código

--- middlewares/validateUrl.js
+++
@@ -1,9 +0,0 @@
-import createShortUrlSchema from "../schemas/createShortUrlSchema.js"
-export default async function validateUrl(req,res,next){
-        const validation = createShortUrlSchema.validate(req.body)
-
-        if(validation.error){
-                return res.status(422).send(validation.error.message)
-        }
-        next()
-}
--- schemas/signInSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-const signInSchema = joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().max(16).required()
-});
-
-export default signInSchema;
